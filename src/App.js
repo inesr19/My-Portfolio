b5eaa3b96b7d41e9a0bd5f1d235b0b2aacb1db73
Ines Radic <inesradic12314@gmail.com>
2021-04-07 13:17:41 -0700

    add path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Homepage from "./components/pages/Homepage";
+import { HashRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Homepage />
+    <HashRouter basename='/'>
+      <div>
+        <ul>
+          <li><Link to='/'>Ines Radic</Link></li>
+        </ul>
+        <hr />
+        <Route exact path='/' component={Homepage} />
+      </div>
+    </HashRouter>
   );
+  
 }
 
 export default App;
