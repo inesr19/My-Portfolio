73edf363895d0b3aa399ccc804f4683e2c2fa2c7
Ines Radic <inesradic12314@gmail.com>
2021-04-09 19:00:27 -0700

    test props with images

--- src/components/Card/CardBody.js
+++ src/components/Card/CardBody.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Card, CardColumns } from 'react-bootstrap';
 import "../Card/style.css"
-import image from "../../assets/images/Profile2.JPG"
 
-function CardBody() {
+
+function CardBody(props) {
     return (
     <CardColumns>
       <Card className="card-body" style={{width: '18rem'}} >
-        <Card.Img variant="top" src={image} />
+        <Card.Img variant="top" src={props.image} />
         <Card.Body>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk
--- src/components/pages/Home/Homepage.js
+++ src/components/pages/Home/Homepage.js
@@ -3,6 +3,8 @@ import NavbarTab from "../../Nav/NavbarTab";
 import CardBody from "../../Card/CardBody"
 import { Col, Container, Row } from "react-bootstrap";
 import Heading from "../../Header";
+import image from "../../../assets/images/Profile2.JPG"
+import notetaker from "../../../assets/gifs/Note-Taker.gif"
 
 function Homepage(props) {
     return (
@@ -14,10 +16,19 @@ function Homepage(props) {
         <Container>
             <Row>
                 <Col>
-                    <CardBody />
+                    <CardBody image={image} src={props.image} />
                 </Col>
             </Row>
         </Container>
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <CardBody image={notetaker} src={props.notetaker}/>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
         </div>
     )
 }
