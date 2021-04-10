33bda1ebf9687de6f13e7a1040d8dfb821929f5f
Ines Radic <inesradic12314@gmail.com>
2021-04-10 10:42:21 -0700

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Homepage from "./components/pages/Home/Homepage";
 import ContactPage from "./components/pages/Contact/ContactPage"
 import PortfolioPage from "./components/pages/Portfolio/PortfolioPage";
@@ -8,11 +8,13 @@ import PortfolioPage from "./components/pages/Portfolio/PortfolioPage";
 
 function App() {
   return (
+    <HashRouter>
     <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/contact" component={ContactPage} />
         <Route exact path="/portfolio" component={PortfolioPage} />
     </Switch>
+    </HashRouter>
   );
   
 }
